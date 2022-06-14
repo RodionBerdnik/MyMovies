d66be4d12c3e7f2e012b0d7d60b608ac31132498
Rodion Berdnik <kasper13082002@gmail.com>
2022-06-14 17:12:57 +0300

    corrected stars

--- js/details.js
+++ js/details.js
@@ -1,4 +1,5 @@
 import { getMovie } from "./api.js";
+import { pipeDuration } from "./pipeDuration.js";
 import { render } from "./render.js";
 
 const movieId = window.location.hash.slice(1)
@@ -14,46 +15,13 @@ getMovie(movieId).then((movie)=>{
 
 
 function renderMovie(movie) {
-    let starIcons = "";
-    if(Number.isInteger(movie.rating)){
-      for (let i = 0; i < 10; i++) {
-        if (movie.rating > i) {
-           starIcons += '<i class="fas fa-star"></i>';
-         }
-         else{
-           starIcons+='<i class="far fa-star"></i>';
-         }
-       }
-    }
-    else{
-      for (let i = 0; i < 10; i++) {
-        if (Math.floor(movie.rating) > i) {
-           starIcons += '<i class="fas fa-star"></i>';
-         }
-         if(Math.floor(movie.rating) == i){
-          starIcons += '<i class="fas fa-star-half-alt"></i>';
-         }
-         if(Math.floor(movie.rating) < i){
-           starIcons+='<i class="far fa-star"></i>';
-         }
-       }
-    }
-    let min=movie.duration
-    let hours 
-    let minutes
-    hours=Math.floor(min/60)
-    if(min%hours==0){
-      minutes=min%60
-    }
-
-
   const movieHTML = `<h1 class="title">${movie.name}</h1>
 <div class="content-wrap">
     <div class="poster-wrap">
         <img src="${movie.poster}" alt="${movie.name}" width="2" height="3"
         decoding="async" class="content-img" />
     <div class="movie-rating">
-        <h3 class="rating-stars">${starIcons}</h3>
+        <h3 class="rating-stars">${createStarIcons(movie.rating)}</h3>
         <h3>Grade: ${movie.rating}</h3>   
     </div>
 </div>
@@ -63,7 +31,7 @@ function renderMovie(movie) {
         <li>Genre:<span class="info"> ${movie.genre}</span></li>
         <li>Director:<span class="info"> ${movie.director}</span></li>
         <li>Actors:<span class="info"> ${movie.actors}</span></li>
-        <li>Duration:<span class="info"> 0${hours}:${minutes}</span></li>
+        <li>Duration:<span class="info"> ${pipeDuration(movie.duration)}</span></li>
         <li>Description:<span class="description"> ${movie.description}</span> </li>
     </ul>
 </div>
@@ -72,4 +40,20 @@ function renderMovie(movie) {
     title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write;
   encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
   render(movieHTML, content);
+}
+
+function createStarIcons(rating) {
+      let starIcons = "";
+  for (let i = 0; i < 10; i++) {
+    if (rating > i + 0.5) {
+       starIcons += '<i class="fas fa-star"></i>';
+     }
+    else if (rating > i) {
+      starIcons += '<i class="fas fa-star-half-alt"></i>';
+    }
+    else{
+        starIcons+='<i class="far fa-star"></i>';
+    }
+  }
+  return starIcons;
 }
